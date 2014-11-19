50784de50dfe8ffc4112002711f8089aa166a6c1
Radek <radek@barca>
2014-11-19 01:29:49 +0100

    dummy commit

--- src/main/java/com/moomeen/endo2java/model/Workout.java
+++ src/main/java/com/moomeen/endo2java/model/Workout.java
@@ -14,7 +14,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Workout {
 
-
 	private Long id;
 	private Duration duration;
 	private Double distance;
