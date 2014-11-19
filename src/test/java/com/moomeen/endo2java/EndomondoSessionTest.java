dff48e1d047d9a085a0809aadfb10240b791222a
Radek <radek@barca>
2014-11-19 01:54:12 +0100

    session test fixes

--- src/test/java/com/moomeen/endo2java/EndomondoSessionTest.java
+++ src/test/java/com/moomeen/endo2java/EndomondoSessionTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import com.moomeen.endo2java.error.InvocationException;
 import com.moomeen.endo2java.error.LoginException;
+import com.moomeen.endo2java.model.AccountInfo;
+import com.moomeen.endo2java.model.DetailedWorkout;
 import com.moomeen.endo2java.model.Workout;
 
 public class EndomondoSessionTest {
@@ -64,23 +66,56 @@ public class EndomondoSessionTest {
 	}
 
 	@Test
-	public void getSingleWorkoutTest(){
-		fail("TODO");
+	public void getSingleWorkoutTest() throws InvocationException {
+		// given
+		EndomondoSession session = new EndomondoSession(EMAIL, PASSWORD);
+
+		// when
+		session.login();
+		List<Workout> list = session.getWorkouts(1);
+		DetailedWorkout workout = session.getWorkout(list.get(0).getId());
+
+		// then
+		assertNotNull(workout);
+		assertEquals(list.get(0).getId(), workout.getId());
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void getSingleWorkoutTest_notLoggedIn() throws InvocationException {
+		long DUMMY_WORKOUT_ID = 666;
+
+		// given
+		EndomondoSession session = new EndomondoSession(EMAIL, PASSWORD);
+
+		// when
+		session.getWorkout(DUMMY_WORKOUT_ID);
+
+		// then exception
 	}
 
 	@Test
-	public void getSingleWorkoutTest_notLoggedIn(){
-		fail("TODO");
+	public void getAccountInfoTest() throws InvocationException {
+		// given
+		EndomondoSession session = new EndomondoSession(EMAIL, PASSWORD);
+
+		// when
+		session.login();
+		AccountInfo info = session.getAccountInfo();
+
+		// then exception
+		assertNotNull(info);
 	}
 
-	@Test
-	public void getAccountInfoTest(){
-		fail("TODO");
-	}
+	@Test(expected = IllegalStateException.class)
+	public void getAccountInfoTest_notLoggedIn() throws InvocationException {
+		// given
+		EndomondoSession session = new EndomondoSession(EMAIL, PASSWORD);
 
-	@Test
-	public void getAccountInfoTest_notLoggedIn(){
-		fail("TODO");
+		// when
+		AccountInfo info = session.getAccountInfo();
+
+		// then exception
+		assertNotNull(info);
 	}
 
 }
